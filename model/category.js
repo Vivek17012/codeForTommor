7d013dfde82c79ea85ba217feb81cc7e183c3272
Vivek17012 <135757246+Vivek17012@users.noreply.github.com>
2024-01-30 23:41:14 +0530

    Update category.js

--- model/category.js
+++ model/category.js
@@ -1,25 +1,23 @@
-//const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-'use strict';
-const { Model } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    static associate(models) {
-      Category.hasMany(models.Service, { foreignKey: 'categoryId' });
-    }
+class Category extends Model {
+  static associate(models) {
+    Category.hasMany(models.Service, { foreignKey: 'categoryId' });
   }
-  Category.init(
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+}
+
+Category.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    {
-      sequelize,
-      modelName: 'Category',
-    }
-  );
-  return Category;
-};
+  },
+  {
+    sequelize,
+    modelName: 'Category',
+  }
+);
+
+module.exports = Category;
