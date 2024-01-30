2d48f207b06e77d7fbadb42aa65455d0cf1a8963
Vivek Shukla <vshukla@neuvays.com>
2024-01-30 21:01:17 +0530

    project work

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('mydb', 'root', 'Vivek123#', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
